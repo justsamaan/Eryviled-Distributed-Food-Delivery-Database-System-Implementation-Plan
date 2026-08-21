5211b0628e0d3f1d08ef5428085399113b105a7b
Mukane Muhammed Samaan <mukanesamaan@gmail.com>
2026-08-21 16:31:33 +0530

    Update base URL for repository name

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Eryviled-/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Eryviled-Distributed-Food-Delivery-Database-System-Implementation-Plan/' : '/',
   plugins: [react()],
   server: {
     port: 3000,
